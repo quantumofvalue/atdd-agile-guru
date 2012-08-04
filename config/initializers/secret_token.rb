ec732c615862e3966cf53fa9ee175ac197840280
Marcin Czenko <marcin@everydayproductive.com>
2012-08-05 01:09:52 +0200

    Rails application generated.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtddAgileGuru::Application.config.secret_token = '51d759aef134a55d4ebb3a4c66e6ab160fbad7b0573760a1d681ab4a66fdfd068b5e97a59acdd816853b862b6d46eae5268067ee623e29cab94cc0e54ed367fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AtddAgileGuru::Application.config.session_store :cookie_store, key: '_atdd-agile-guru_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AtddAgileGuru::Application.config.session_store :active_record_store
